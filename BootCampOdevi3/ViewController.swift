b597a0197435a5bf986476cfec3817ecc89571ca
ahmetkardesseven <ahmetkardesseven@gmail.com>
2023-01-21 23:21:44 +0300

    Initial Commit

---
+++ BootCampOdevi3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BootCampOdevi3
+//
+//  Created by ahmet kardesseven on 21.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+
+
+}
+
