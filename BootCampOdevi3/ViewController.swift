722a9db6e77dd23501f2c34d23a3c5b3dcc41799
ahmetkardesseven <ahmetkardesseven@gmail.com>
2023-01-22 13:18:15 +0300

    Bitti

--- BootCampOdevi3/ViewController.swift
+++ BootCampOdevi3/ViewController.swift
@@ -11,7 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        self.navigationItem.title = "Saat"
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor.systemBlue
+        appearance.titleTextAttributes = [.foregroundColor:UIColor.white, .font:UIFont(name: "Solitreo-Regular", size: 22)]
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
     }
 
 
